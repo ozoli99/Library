80f87506c92b4fb810b4952c380acc9e1b7aa5d4
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-04 20:15:36 +0200

    Implement Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
+    function info() {
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read ? "read" : "not read yet";
+    }
+}
