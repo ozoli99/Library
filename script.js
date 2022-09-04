2842e2685a0b6c8005fb45530bed045e1886c492
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-04 20:35:58 +0200

    Create Library class and convert Book object to class

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title = "Unknown", author = "Unknown", pages = 0, isRead = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+}
 
-    function info() {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read ? "read" : "not read yet";
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(newBook) {
+        if (!this.isInLibrary(newBook)) {
+            this.books.push(newBook);
+        }
+    }
+
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title);
+    }
+
+    getBook(title) {
+        return this.books.find((book) => book.title === title);
+    }
+
+    isInLibrary(newBook) {
+        return this.books.some((book) => book.title === newBook.title);
     }
 }
