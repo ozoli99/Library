a090c11c2da82ce185d7a746e44537dd388217dc
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-04 21:58:20 +0200

    Implement util functions

--- script.js
+++ script.js
@@ -284,3 +284,26 @@ const getBookIdDB = async (title) => {
     const bookId = snapshot.docs.map((doc) => doc.id).join('');
     return bookId;
 };
+
+// Utils
+
+const docsToBooks = (docs) => {
+    return docs.map((doc) => {
+        return new Book(doc.data().title, doc.data().author, doc.data().pages, doc.data().isRead)
+    });
+};
+
+const JSONToBook = (book) => {
+    return new Book(book.title, book.author, book.pages, book.isRead);
+};
+
+const bookToDoc = (book) => {
+    return {
+        ownerId: auth.currentUser.uid,
+        title: book.title,
+        author: book.author,
+        pages: book.pages,
+        isRead: book.isRead,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    };
+};
